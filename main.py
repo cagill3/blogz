a22143c6f246f751fba07d8f4b173ca764afab01
Carl Gillis <carlgillis@Carls-MBP.attlocal.net>
2018-04-14 22:45:43 -0500

    Added error messaging

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from flask import Flask, request, redirect, render_template
+from flask import Flask, request, redirect, render_template, flash
 from flask_sqlalchemy import SQLAlchemy
 import cgi
 import flask
-#from flask import Session
+
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -33,12 +33,21 @@ def index():
     if request.method == 'POST':
         post_title = request.form['post-title']
         post_body = request.form['post-body']
-        new_post = Blog(post_title, post_body)
-        db.session.add(new_post)
-        db.session.commit()
+        if post_title == '':
+            title_error = 'Please fill in the title'
+            return render_template('new_posts.html', title='Add Blog Entry',
+                                    title_error=title_error)
+        if post_body == '':
+            body_error = 'Please fill in the body'
+            return render_template('new_posts.html', title='Add Blog Entry',
+                                    body_error=body_error)
+        else:
+            new_post = Blog(post_title, post_body)
+            db.session.add(new_post)
+            db.session.commit()
 
-        posts = Blog.query.all()
-        return render_template('blog_entry.html', posts=posts)
+            posts = Blog.query.all()
+            return render_template('blog_entry.html', posts=posts)
 
 
 @app.route('/blog', methods=['POST','GET'])
